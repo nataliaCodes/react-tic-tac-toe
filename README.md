5d5d941b1d7421b9cac32969b11aaa2c51a3daf5
Natalia M <natalia.martian@gmail.com>
2023-06-14 23:03:58 -0400

    Resolve requirement 2: Rewrite Board to use a loop to make the squares instead of hardcoding them.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Starting point:
 
 Requirements:
 1. For the current move only, show “You are at move #…” instead of a button.
-2. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+2. Rewrite Board to use two loops to make the squares instead of hardcoding them. (I actually used one loop as I did not see the need for the second one).
 3. Add a toggle button that lets you sort the moves in either ascending or descending order.
 4. When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
 5. Display the location for each move in the format (row, col) in the move history list.
--- src/App.js
+++ src/App.js
@@ -30,24 +30,30 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
+  let groupedSquares = [];
+  let row = [];
+  let squareNumber = 1;
+  for (let i=0; i < squares.length; i++) {
+    row.push(
+      <Square 
+        key={i}
+        value={squares[i]} 
+        onSquareClick={() => handleClick(i)} 
+      />
+    );
+    squareNumber++;
+
+    if (squareNumber === 4) {
+      groupedSquares.push(<div key={i} className="board-row">{row}</div>);
+      squareNumber = 1;
+      row = [];
+    }
+  }
+
   return (
     <Fragment>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {groupedSquares}
     </Fragment>
   );
 }
